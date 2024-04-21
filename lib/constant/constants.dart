a59c9034661b443313a2af4914bd298b7eeaefda
Shantanu Swami <57904885+jaibhedia@users.noreply.github.com>
2024-04-22 00:55:16 +0530

    Update constants.dart

--- lib/constant/constants.dart
+++ lib/constant/constants.dart
@@ -1,13 +1,13 @@
 String mapBoxApiKey =
-    'pk.eyJ1Ijoic2hhbnRhbnVjc2QiLCJhIjoiY2xxaTRqaTBwMWduazJ1bWs4OTFyYnBqMCJ9.3CwUjdQ3OZfLV10MGKDFMA';
+    '';
 
 String nftStorageApiKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDBkODk0MkFGRDI4QUU0NjMzNjc5MEJjQzU0MzVhZTQ5NzAyNEU5MjMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTcwMzMzOTk0MTAwNiwibmFtZSI6ImxhbmQxIn0.YK9LPFc-Gu5o23WCBgecK4mm8PHxx1Cjc_3Iduq3Y1o';
+    '';
 
 const String rpcUrl =
     //"https://rpc-mumbai.maticvigil.com/v1/e5c1c2d68fa03cce040bae393b8193a7d100fe7b";
 "http://127.0.0.1:7545";
 
-const String contractAddress = "0xFBA25f71716ff500801Ccc8dcE90a335C012978B";
+const String contractAddress = "";
 
 const int chainId = 1337; //Polygon testnet-80001, local ganache-1337
